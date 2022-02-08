adf5abc298f06f543f7f368a548a15347f52b038
salvador <atagong5@gmail.com>
2022-02-08 05:03:24 +0100

    deplyment scrp updated

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 # creating virtual environment
 echo "... creating virtual environment"
 python3 -m venv venv
-test $? = 1 && echo "... virtual environment created"
+test $? = 0 && echo "... virtual environment created"
 # activating virtual environment
 source venv/bin/activate
 pip install --upgrade pip > /dev/null
-test $? = 1 &&  echo "... virtual environment activated"
+test $? = 0 && echo "... virtual environment activated"
 # installing requirements
 pip3 install -r requirements.txt > /dev/null
-test $? = 1 && echo "... requirements installed"
+test $? = 0 && echo "... requirements installed"
 # make migration
 ./manage.py makemigration > /dev/null
 # migrate
 ./manage.py migrate > /dev/null
 # launch server
 ./manage.py runserver 0.0.0.0:8000 > /dev/null
-test $? = 1 && echo "... project running at http://<your_ip_address>:8000"
+test $? = 0 && echo "... project running at http://<your_ip_address>:8000"
