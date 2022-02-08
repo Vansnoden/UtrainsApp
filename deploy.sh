3dd4f3f28287b4de0b928302977b74e19e1b029d
salvador <atagong5@gmail.com>
2022-02-08 04:59:28 +0100

    deployment script added

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+# creating virtual environment
+echo "... creating virtual environment"
+python3 -m venv venv
+test $? = 1 && echo "... virtual environment created"
+# activating virtual environment
+source venv/bin/activate
+pip install --upgrade pip > /dev/null
+test $? = 1 &&  echo "... virtual environment activated"
+# installing requirements
+pip3 install -r requirements.txt > /dev/null
+test $? = 1 && echo "... requirements installed"
+# make migration
+./manage.py makemigration > /dev/null
+# migrate
+./manage.py migrate > /dev/null
+# launch server
+./manage.py runserver 0.0.0.0:8000 > /dev/null
+test $? = 1 && echo "... project running at http://<your_ip_address>:8000"
