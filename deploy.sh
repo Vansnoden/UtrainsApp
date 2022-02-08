127c6243841ec16a63bd4c9ced3d67e1a0082523
salvador <atagong5@gmail.com>
2022-02-08 05:07:52 +0100

    script bug resolved

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ echo "... creating virtual environment"
 python3 -m venv venv
 test $? = 0 && echo "... virtual environment created"
 # activating virtual environment
-source venv/bin/activate
+source venv/bin/activate > /dev/null
 pip install --upgrade pip > /dev/null
 test $? = 0 && echo "... virtual environment activated"
 # installing requirements
 pip3 install -r requirements.txt > /dev/null
 test $? = 0 && echo "... requirements installed"
 # make migration
-./manage.py makemigration > /dev/null
+./manage.py makemigrations > /dev/null
 # migrate
 ./manage.py migrate > /dev/null
 # launch server
